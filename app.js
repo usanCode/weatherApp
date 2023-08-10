5eefe51fbe4885d05a87f1f96f795a3079d62451
usanCode <sabinerugugula@gmail.com>
2023-08-10 16:42:16 +0200

    changing 3.0 to 2.5

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 
 let apiKey = "bd652b91d2fb3900a40f02b4f3dd58fb";
-let apiUrl = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={bd652b91d2fb3900a40f02b4f3dd58fb}" ;
+let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={bd652b91d2fb3900a40f02b4f3dd58fb}" ;
 console.log(apiUrl);
